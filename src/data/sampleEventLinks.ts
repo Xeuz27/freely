878df0040257614aa436d482bfd01c154cc10652
Jesus Gutierrez - Xeuz27 <yidev27@gmail.com>
2026-04-15 21:02:52 -0400

    chore: sample event links

---
+++ src/data/sampleEventLinks.ts
@@ -0,0 +1,14 @@
+import type { eventLink } from '@/types/calendar-types'
+
+export const sampleEventLinks: eventLink[] = [
+	{
+		id: 'abcdefg',
+		eventId: '1',
+		leadId: '1'
+	},
+	{
+		id: 'jklasd',
+		eventId: '3d388570',
+		leadId: '066ef190'
+	}
+]
